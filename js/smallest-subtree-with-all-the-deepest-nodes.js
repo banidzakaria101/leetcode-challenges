7599ea67f63d6d5c69e22271c232a9f9bcb26d71
zakariabanid101 <banidzakaria2@gmail.com>
2026-01-09 23:08:57 +0100

    smallest-subtree-with-all-the-deepest-nodes

---
+++ js/smallest-subtree-with-all-the-deepest-nodes.js
@@ -0,0 +1,21 @@
+var subtreeWithAllDeepest = function(root) {
+
+  function dfs(node) {
+    if (!node) return [0, null];
+
+    let [leftDepth, leftNode] = dfs(node.left);
+    let [rightDepth, rightNode] = dfs(node.right);
+
+    if (leftDepth > rightDepth) {
+      return [leftDepth + 1, leftNode];
+    }
+    if (rightDepth > leftDepth) {
+      return [rightDepth + 1, rightNode];
+    }
+
+    // equal depth → this node is LCA of deepest nodes
+    return [leftDepth + 1, node];
+  }
+
+  return dfs(root)[1];
+};
